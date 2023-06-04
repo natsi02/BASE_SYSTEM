f7012a1a041fd0d4200f602a6da83af8b55a456c
natsi02 <natchanons2002@gmail.com>
2023-06-04 18:09:47 +0700

    Comm->EndEffector_V1

--- Core/Inc/I2C_EndEffector.h
+++ Core/Inc/I2C_EndEffector.h
@@ -20,9 +20,6 @@ void ENDEFF_GRIPPER_RUNMODE(I2C_HandleTypeDef* hi2c);
 void ENDEFF_GRIPPER_IDLE(I2C_HandleTypeDef* hi2c);
 void ENDEFF_GRIPPER_PICK(I2C_HandleTypeDef* hi2c);
 void ENDEFF_GRIPPER_PLACE(I2C_HandleTypeDef* hi2c);
-//DecimalToBinary
-void decimalToBinary(uint8_t decimal);
 //I2C Comm with base system
 void I2C_TO_BASESYSTEM(uint8_t DataFrame,I2C_HandleTypeDef *hi2c);
-void TransmitToBase(char* Data,uint8_t result);
 #endif /* INC_I2C_ENDEFFECTOR_H_ */
