a9fa6dc5bfc9e69d2e4599188e46570e5cad4045
natsi02 <natchanons2002@gmail.com>
2023-06-06 19:31:04 +0700

    Joystick->comm F411RE_V4

--- Core/Src/Joystick.c
+++ Core/Src/Joystick.c
@@ -7,26 +7,15 @@
 #include "Joystick.h"
 #include "usart.h"
 
-#define BUFFER_SIZE 6
 uint8_t RxBuffer[1];
 
 void UARTInterruptConfig() {
 	HAL_UART_Receive_IT(&huart1, RxBuffer, 1);
 }
 
-//void Joystick_Received(int* receivedByte){
-//	if(RxBuffer[0] == 1 && RxBuffer[5] == 1)
-//	{
-//		for(int i = 0; i< BUFFER_SIZE;i++) receivedByte[i] = RxBuffer[i];
-//		if(RxBuffer[1] > UINT8_MAX/2) receivedByte[1] -= UINT8_MAX+1;
-//		if(RxBuffer[2] > UINT8_MAX/2) receivedByte[2] -= UINT8_MAX+1;
-//	}
-//	else for(int j = 0; j< BUFFER_SIZE;j++) RxBuffer[j] = 0;
-//	HAL_UART_Receive_IT(&huart1, RxBuffer, sizeof(RxBuffer));
-//}
-
 void Joystick_Received(int *receivedByte) {
 	static int count;
+	static uint8_t tempData[6];
 	static enum {
 		START, COUNT
 	} Joy_State = START;
@@ -39,23 +28,27 @@ void Joystick_Received(int *receivedByte) {
 
 	case (COUNT):
 		if (RxBuffer[0] == 69) {
-			for (int i = 0; i < sizeof(receivedByte); i++) {
-				receivedByte[i] = 0;
+			for (int i = 0; i < sizeof(tempData); i++) {
+				tempData[i] = 0;
 			}
 			count = 0;
-		} else if (RxBuffer[0] == 71 && count < 4) {
-			for (int i = 0; i < sizeof(receivedByte); i++) {
-				receivedByte[i] = 0;
+		} else if (RxBuffer[0] == 71 && count < sizeof(tempData)) {
+			for (int i = 0; i < sizeof(tempData); i++) {
+				tempData[i] = 0;
 			}
 			count = 0;
-		} else if (RxBuffer[0] == 71 && count == 4) {
+		} else if (RxBuffer[0] == 71 && count == 6) {
 			count = 0;
+			receivedByte[0] = (tempData[1] << 8) | tempData[0];
+			receivedByte[1] = (tempData[3] << 8) | tempData[2];
+			receivedByte[2] = tempData[4];
+			receivedByte[3] = tempData[5];
+			if(receivedByte[0] > UINT16_MAX/2) receivedByte[0] -= UINT16_MAX + 1;
+			else if(receivedByte[1] > UINT16_MAX/2) receivedByte[1] -= UINT16_MAX + 1;
 			Joy_State = START;
 			// All data received
 		} else {
-			receivedByte[count] = RxBuffer[0];
-			if (receivedByte[count] > UINT8_MAX / 2)
-				receivedByte[count] -= UINT8_MAX + 1;
+			tempData[count] = RxBuffer[0];
 			count++;
 		}
 		break;
